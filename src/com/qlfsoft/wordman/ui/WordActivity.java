2ed471592c2e8efc9349376b3c3fd8db73bd2ad0
Administrator <Administrator@10.33.83.222>
2016-01-13 16:54:13 +0800

    160113

---
+++ src/com/qlfsoft/wordman/ui/WordActivity.java
@@ -0,0 +1,34 @@
+package com.qlfsoft.wordman.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class WordActivity extends BaseActivity {
+
+	private ImageButton ib_menu;
+	private TextView tv_log;
+	private Button btn_sound;
+	private TextView tv_word;
+	private TextView tv_phonetic;
+	private RelativeLayout rl_new;
+	private TextView tv_description;
+	private Button btn_next1;
+	private Button btn_konw;
+	private Button btn_unknow;
+	private LinearLayout ll_review;
+	private ListView lv_selectors;
+	private Button btn_next2;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
